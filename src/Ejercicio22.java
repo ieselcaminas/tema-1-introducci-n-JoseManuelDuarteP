c11398537eccab09cc6f1da61c5ee646d64d38a9
josduaper <josduaper@alu.edu.gva.es>
2024-09-20 11:40:27 +0200

    Prueba&Ejer22

---
+++ src/Ejercicio22.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        int num;
+        int positivos = 0;
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0 ; i < 10 ; i++) {
+            System.out.println("Ingrese un numero: ");
+            num = sc.nextInt();
+
+            if (num > 0) {
+                ++positivos;
+            }
+        }
+
+        System.out.println("Positivos: " + positivos);
+
+        sc.close();
+    }
+}
