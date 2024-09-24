6ff6dd549e80c7aaa918aa1b3f11922f31dc88ba
josduaper <josduaper@alu.edu.gva.es>
2024-09-24 12:44:20 +0200

    Ejer25

---
+++ src/Ejercicio25.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        long factorial = 1;
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero: ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("El factorial del numero " + num + " es " + factorial);
+
+        sc.close();
+    }
+}
