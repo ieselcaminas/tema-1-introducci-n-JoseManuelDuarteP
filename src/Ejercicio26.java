4eb5c67859a5f28026f945c82b45a97990c226aa
josduaper <josduaper@alu.edu.gva.es>
2024-09-24 12:51:52 +0200

    Ejer26

---
+++ src/Ejercicio26.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ejercicio26 {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero: ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+        sc.close();
+    }
+}
