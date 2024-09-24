8ea3ce2d8fe80bf7d8cf60b28ac4da854eed4bd8
josduaper <josduaper@alu.edu.gva.es>
2024-09-24 13:32:31 +0200

    Ejer31

---
+++ src/Ejercicio31.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ejercicio31 {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero: ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
