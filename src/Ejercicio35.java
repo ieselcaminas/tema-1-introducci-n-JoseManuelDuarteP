56f91f23d313e16f8d74219ec521271a8e9733e8
josduaper <josduaper@alu.edu.gva.es>
2024-09-26 11:47:54 +0200

    Ejer35

---
+++ src/Ejercicio35.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ejercicio35 {
+    public static void main(String[] args) {
+        int num;
+        int num2;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero: ");
+        num = sc.nextInt();
+
+        System.out.print("Ingrese por cuanto quiere dividir: ");
+        num2 = sc.nextInt();
+
+        do {
+            num = num / num2;
+            System.out.println(num);
+        } while (num > num2);
+
+        sc.close();
+    }
+}
