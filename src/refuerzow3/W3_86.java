7d09bf9e67306d0f419b9f41c20f4ec6b95355c8
josduaper <josduaper@alu.edu.gva.es>
2024-09-26 13:41:50 +0200

    W3-86

---
+++ src/refuerzow3/W3_86.java
@@ -0,0 +1,27 @@
+package refuerzow3;
+
+import java.util.Scanner;
+
+public class W3_86 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero: ");
+        n = sc.nextInt();
+
+        do {
+            if (n % 2 == 0) {
+                n /= 2;
+                System.out.println(n);
+            } else {
+                n = (n*3) +1;
+                System.out.println(n);
+            }
+        } while (n != 1);
+
+        System.out.println("Ahora es: " + n);
+
+        sc.close();
+    }
+}
