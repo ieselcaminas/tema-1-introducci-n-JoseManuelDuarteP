d21dfed56b45b22ef86e30e457826207363c66ee
josduaper <josduaper@alu.edu.gva.es>
2024-10-04 10:41:57 +0200

    Ejercicio37

--- src/introduccion/Ejercicio37.java
+++ src/introduccion/Ejercicio37.java
@@ -5,9 +5,22 @@ import java.util.Scanner;
 public class Ejercicio37 {
     public static void main(String[] args) {
         int numUsuario;
+        int decimal = 0;
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Ingrese un número binario: ");
+        numUsuario = sc.nextInt();
+        int numeroDeDigitos = (int) Math.log10(numUsuario) + 1;
 
+        for (int i = 0; i <= numeroDeDigitos; i++) {
+
+            if (numUsuario % 10 == 1) {
+                decimal = decimal + (int) Math.pow(2, i);
+            }
+            numUsuario = numUsuario / 10;
+
+        }
+        System.out.println("El número en decimal es: " + decimal);
 
         sc.close();
     }
