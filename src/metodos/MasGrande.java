a94a14946c34f725ef3f78c417cb9788b9f5ac81
josduaper <josduaper@alu.edu.gva.es>
2024-10-10 13:51:15 +0200

    metodos

---
+++ src/metodos/MasGrande.java
@@ -0,0 +1,16 @@
+package metodos;
+
+//Diseña un algoritmo que lea dos valores y muestre el más grande de ellos.
+public class MasGrande {
+    public static void main(String argv[]) {
+        int numero = Utilidades.leerEntero("Introduce un número entero:");
+        int numero2 = Utilidades.leerEntero("Introduce otro número entero:");
+
+        // Si las condiciones son mutuamente excluyentes, se usa if .. else
+        if (numero > numero2) {
+            System.out.println("El número más grande es: " + numero);
+        } else {
+            System.out.println("El número más grande es: " + numero2);
+        }
+    }
+}
